0f8a7e621db7de3af744f167861e78ed69ac9643
name296 <rkdcjf0655@inu.ac.kr>
2026-01-22 16:45:25 +0900

    리팩터링전 마지막2

--- src/components/contents/screens/ScreenSimplePay.js
+++ src/components/contents/screens/ScreenSimplePay.js
@@ -21,9 +21,10 @@ const ScreenSimplePay = memo(() => {
                 <div>오른쪽 아래에 있는 <span className="primary">QR리더기</span>에</div>
                 <div><span className="primary">QR코드</span>를 인식시킵니다</div>
             </div>
-            <img src="./images/device-codeReader-simple.png" alt="" className="credit-pay-image" onClick={() => navigateTo('ScreenOrderComplete')} />
+            <img src="./images/device-codeReader-simple.png" alt="" className="credit-pay-image" />
             <div ref={actionBarRef} className="task-manager">
-                <Button className="w500h120" navigate="ScreenPayments" label="취소" />
+                <Button className="w371h120" navigate="ScreenPayments" label="취소" />
+                <Button className="w371h120" navigate="ScreenOrderComplete" label="가상인식" />
             </div>
         </>
     );
